e2be2ffa20df27ef3a1a88c15d61a1cbd95fd176
roldaojr <roldaogjr@gmail.com>
2014-07-08 19:59:13 -0300

    Habilitada autentição na API REST

--- siade/settings/common.py
+++ siade/settings/common.py
@@ -70,7 +70,12 @@ STATIC_ROOT = 'staticfiles'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+	'DEFAULT_FILTER_BACKENDS': (
+		'rest_framework.filters.DjangoFilterBackend',
+	),
+	'DEFAULT_PERMISSION_CLASSES': (
+		'rest_framework.permissions.DjangoModelPermissions',
+	),
 	'PAGINATE_BY': 10,                 # Default to 10
 	'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
 	'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
