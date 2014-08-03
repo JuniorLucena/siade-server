443dd3f1afeb50c3c9693fbd0ce1db597fcf2d44
roldaojr <roldaogjr@gmail.com>
2014-08-03 15:54:57 -0300

    Removido django-debug-toolbar

--- siade/settings/development.py
+++ siade/settings/development.py
@@ -6,7 +6,6 @@ from .common import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
-INSTALLED_APPS = list(INSTALLED_APPS) + ['debug_toolbar']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
