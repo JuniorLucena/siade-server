9c033e0f2f6f6b90b86ec6f1d30f6b0b6f6213cc
roldaojr <roldaogjr@gmail.com>
2014-08-02 22:04:54 -0300

    Ainda remendo código antigo

--- siade/urls.py
+++ siade/urls.py
@@ -7,7 +7,6 @@ xadmin.autodiscover()
 urlpatterns = patterns('',
 	url(r'^$', TemplateView.as_view(template_name='base_site.html')),
 	url(r'^admin/', include(xadmin.site.urls)),
-	url(r'^imoveis/', include('siade.imoveis.urls')),
 	url(r'^api/', include('siade.api.urls')),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 	url(r'^o/', include('oauth2_provider.urls')),
