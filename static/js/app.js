efd566e9b6c6c4e5d26a74446feebfd322ab4f50
luziana <luzi.feitosa@hotmail.com>
2014-08-06 09:47:17 -0300

    fazendo requisicoes CRUD

--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,27 @@ siadeApp.config(['$routeProvider', function($routeProvider) {
 		templateUrl: DJANGO_STATIC_URL+'partials/home.html',
 		controller: 'homeCtrl'
 	})
+	.when('/estados', {
+		templateUrl: DJANGO_STATIC_URL+'partials/listar_uf.html',
+		controller: 'estadoCtrl'
+	})
+	.when('/cadastrar_uf', {
+		templateUrl: DJANGO_STATIC_URL+'partials/cadastrar_uf.html',
+		controller: 'estadoCtrl'
+	})
+	.when('/edit_uf', {
+		templateUrl: DJANGO_STATIC_URL+'partials/edit_uf.html',
+		controller: 'estadoCtrl'
+	})
+	.when('/logradouros', {
+		templateUrl: DJANGO_STATIC_URL+'partials/cadastrar_logradouro.html',
+		controller: 'logradouroCtrl'
+	})
+	
+	.when('/cidades', {
+		templateUrl: DJANGO_STATIC_URL+'partials/cidade.html',
+		controller: 'cidadeCtrl'
+	})
 	.when('/bairros', {
 		templateUrl: DJANGO_STATIC_URL+'partials/bairro.html',
 		controller: 'bairroCtrl'
