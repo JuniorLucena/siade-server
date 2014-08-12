4e7708d5c0ac74c171dc81f4bc89e6a22c0f31b8
luziana <luzi.feitosa@hotmail.com>
2014-08-12 09:31:52 -0300

    crud

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,8 @@
 'use strict'
 var siadeApp = angular.module('siadeApp', [
 	'ngRoute',
-	'siadeControllers'
+	'siadeControllers',
+	'ui.select2'
 ])
 siadeApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
@@ -15,24 +16,36 @@ siadeApp.config(['$routeProvider', function($routeProvider) {
 	})
 	.when('/cadastrar_uf', {
 		templateUrl: DJANGO_STATIC_URL+'partials/cadastrar_uf.html',
-		controller: 'estadoCtrl'
+		controller: 'estado_Cadastro_Ctrl'
 	})
 	.when('/edit_uf', {
 		templateUrl: DJANGO_STATIC_URL+'partials/edit_uf.html',
-		controller: 'estadoCtrl'
+		controller: 'estadoEditCtrl'
 	})
 	.when('/logradouros', {
-		templateUrl: DJANGO_STATIC_URL+'partials/cadastrar_logradouro.html',
+		templateUrl: DJANGO_STATIC_URL+'partials/listar_logradouro.html',
 		controller: 'logradouroCtrl'
+	})
+	.when('/cadastrar_logradouro', {
+		templateUrl: DJANGO_STATIC_URL+'partials/listar_logradouro.html',
+		controller: 'cadastrar_logradouro_Ctrl'
 	})
 	
 	.when('/cidades', {
-		templateUrl: DJANGO_STATIC_URL+'partials/cidade.html',
+		templateUrl: DJANGO_STATIC_URL+'partials/listar_cidade.html',
 		controller: 'cidadeCtrl'
 	})
+	.when('/cadastrar_cidade/', {
+		templateUrl: DJANGO_STATIC_URL+'partials/cadastrar_cidade.html',
+		controller: 'cidade_Cadastro_Ctrl'
+	})
 	.when('/bairros', {
-		templateUrl: DJANGO_STATIC_URL+'partials/bairro.html',
+		templateUrl: DJANGO_STATIC_URL+'partials/listar_bairro.html',
 		controller: 'bairroCtrl'
+	})
+	.when('/cadastrar_bairro', {
+		templateUrl: DJANGO_STATIC_URL+'partials/cadastrar_bairro.html',
+		controller: 'cadastrar_bairro_Ctrl'
 	})
 	.when('/quadras', {
 		templateUrl: DJANGO_STATIC_URL+'partials/quadra.html',
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -5,17 +5,63 @@ siadeCtrls.controller('homeCtrl', ['$scope', function($scope) {
 	$scope.valor = 1
 }])
 
-siadeCtrls.controller('bairroCtrl', ['$scope', function($scope) {
+siadeCtrls.controller('bairroCtrl', ['$scope', '$http', '$window', '$location', function($scope,$http,$window,$location) {
+
+	$scope.addBairro = function(){
+		$location.path('/cadastrar_bairro/')
+	}
 	
+	$scope.editUf = function(bairro){
+		$location.path('/edit_bairro/'+ bairro)
+	}	
+	
+ 	
+	var init = function(){
+		$http.get('/api/imoveis/bairro')
+		.success(function (data){
+			$scope.lista = data
+		}).error(function(data){
+			alert("erro no angularjs!")		
+		})
+ 	   
+	}
+	
+	init();
       
 }])
 
-siadeCtrls.controller('cidadeCtrl', ['$scope','$http', '$window', function ($scope,$http,$window) {
+siadeCtrls.controller('cidadeCtrl', ['$scope','$http', '$window', '$location', function ($scope,$http,$window,$location) {
+	
+	$scope.addCidade = function(){
+		$location.path('/cadastrar_cidade/')
+	}
+	
+	$scope.editUf = function(municipio){
+		$location.path('/edit_municipio/'+ municipio)
+	}	
+	
+ 	
+	var init = function(){
+		$http.get('/api/imoveis/municipio')
+		.success(function (data){
+			$scope.lista = data
+		}).error(function(data){
+			alert("erro no angularjs!")		
+		})
+ 	   
+	}
+	
+	init();
+}])
+
+
+siadeCtrls.controller('cidade_Cadastro_Ctrl', ['$scope','$http', '$location', function ($scope,$http,$location) {
+
 	$scope.municipio ={};
 
 	$scope.save = function(){
 		
-		$http.post('/api/imoveis/municipio', $scope.municipio)
+		$http.post('/api/imoveis/municipio/', $scope.municipio)
 		.success(function (data){
 			console.log(data)
 			}).error(function(data){
@@ -29,46 +75,47 @@ siadeCtrls.controller('cidadeCtrl', ['$scope','$http', '$window', function ($sco
 	}	
 
 
-$http.get('/api/imoveis/municipio')
+	var init = function(){
+		$http.get('/api/imoveis/municipio')
 		.success(function (data){
-			console.log(data)
+			$scope.lista = data
 		}).error(function(data){
 			alert("erro no angularjs!")		
 		})
- 
-
-
+ 	   
+	}
+	
+	init();
 }])
+
 .controller('quadraCtrl', ['$scope', function($scope) {
 	$scope.valor = 1
 }])
+
+
+
 .controller('imovelCtrl', ['$scope', function($scope) {
 	$scope.valor = 1
 }])
 
 
-siadeCtrls.controller('estadoCtrl', ['$scope','$http', '$location', function ($scope,$http,$location) {
-	$scope.uf ={};
+siadeCtrls.controller('estadoCtrl', ['$scope','$http', '$location', '$window', function ($scope,$http,$location,$window) {
+	
 
-	
-	$scope.saveUf = function(){
-		
-		$http.post('/api/imoveis/uf', $scope.uf)
-		.success(function (data){
-			$scope.lista.unshift(data)
-		}).error(function(data){
-			alert("erro no codigo!")		
-		})
-	
-	}
 	$scope.addUf = function(){
 		$location.path('/cadastrar_uf/')
 	}
 	
-	$scope.editUf = function(uf){
-		$location.path('/edit_uf/'+ uf    		)
-	}	
-	
+	$scope.excluir = function(uf){
+		var confirm = $window.confirm('Tem certeza que deseja excluir o produto '+ uf.id+ '?');
+		if(confirm){
+			$http.delete('/api/imoveis/uf/'+uf.id).success(function(data){
+				var index = $scope.lista.indexOf(uf);
+				$scope.lista.splice(index, 1);
+			});
+		}
+	};
+
  	
 	var init = function(){
 		$http.get('/api/imoveis/uf')
@@ -82,7 +129,73 @@ siadeCtrls.controller('estadoCtrl', ['$scope','$http', '$location', function ($s
 	
 	init();
 
+}])
+
+siadeCtrls.controller('estadoDelCtrl', ['$scope', '$location', '$http', function ($scope, $http, $location) {
+
+       
+         
+}]);
+
+siadeCtrls.controller('estadoEditCtrl', ['$scope', '$location', '$http', function ($scope, $http, $location) {
+
+      
+        $scope.updateUf = function (uf) {
+            $scope.update($scope.uf);
+            $location.path('/estados');
+        };
+
+         
+}]);
+
+
+siadeCtrls.controller('estado_Cadastro_Ctrl', ['$scope','$http', '$location', function ($scope,$http,$location) {
+	$scope.uf ={};
+
+	$scope.saveUf = function(){
+		
+		$http.post('/api/imoveis/uf/', $scope.uf)
+		.success(function (data){
+			alert("post ")
+			$scope.lista.unshift(data)
+		}).error(function(data){
+			alert("erro no codigo!")		
+		})
+	
+	}
+		
+	var init = function(){
+		$http.get('/api/imoveis/uf')
+		.success(function (data){
+			$scope.lista = data
+		}).error(function(data){
+			alert("erro no angularjs!")		
+		})
+ 	   
+	}
+	
+	init();
+
+}])
+
+
+siadeCtrls.controller('logradouroCtrl', ['$scope','$http', '$location', function ($scope,$http,$location) {
 	
 
+	$scope.addUf = function(){
+		$location.path('/cadastrar_logradouro/')
+	}
+		
+	var init = function(){
+		$http.get('/api/imoveis/logradouro')
+		.success(function (data){
+			$scope.lista = data
+		}).error(function(data){
+			alert("erro no angularjs!")		
+		})
+ 	   
+	}
+	
+	init();
 
 }])
