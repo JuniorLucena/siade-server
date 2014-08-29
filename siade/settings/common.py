300a7cab3969949117dd02830dac10925a8c14e6
Roldão Jr <roldaogjr@gmail.com>
2014-08-29 19:17:48 -0300

    Modificado settings

--- siade/settings/common.py
+++ siade/settings/common.py
@@ -84,8 +84,8 @@ REST_FRAMEWORK = {
         'rest_framework.filters.DjangoFilterBackend',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissions',
