0c7ea71dda6e8dae718a3aa8325ca60741404a4b
Roldão Rego Jr <roldao.rego@ifrn.edu.br>
2014-08-29 09:38:23 -0300

    Config de log do Heroku

--- siade/settings/production.py
+++ siade/settings/production.py
@@ -10,5 +10,20 @@ ALLOWED_HOSTS = ['*']
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 INSTALLED_APPS = list(INSTALLED_APPS) + ['gunicorn']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#DEBUG = False
-#TEMPLATE_DEBUG = False
+DEBUG = os.environ.get('DEBUG', True)
+TEMPLATE_DEBUG = DEBUG
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+        }
+    }
+}
