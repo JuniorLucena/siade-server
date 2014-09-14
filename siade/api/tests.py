082df78a68f4d6d1533aab2202c22861a42ecfda
roldaojr <roldaogjr@gmail.com>
2014-09-13 22:53:41 -0300

    Atualizado tests.py

--- siade/api/tests.py
+++ siade/api/tests.py
@@ -3,20 +3,18 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
 from django.db.models.loading import get_model
 from rest_framework.test import APITestCase
-from simple_history.models import HistoricalRecords
 
 
 class ApiTest(APITestCase):
     def setUp(self):
         User = get_user_model()
         self.user = User.objects.create_superuser(
-            'testuser',
-            email='testuser@test.com',
+            cpf=12345678909, nome='Test', sobrenome='User',
             password='testing'
         )
 
     def _doLogin(self):
-        self.client.login(username='testuser', password='testing')
+        self.client.login(cpf=12345678909, password='testing')
 
     def test_adicionar_uf_sem_autenticar(self):
         uflist = reverse('uf-list')
@@ -45,25 +43,21 @@ class SyncTest(APITestCase):
         # autenticar-se
         User = get_user_model()
         self.user = User.objects.create_superuser(
-            'testuser', email='testuser@test.com', password='testing'
+            cpf=12345678909, nome='Test', sobrenome='User',
+            password='testing'
         )
-        self.client.login(username='testuser', password='testing')
-        # definir usuário para o django-simple-history
-        HistoricalRecords.thread._history_user = self.user
+        self.client.login(cpf=12345678909, password='testing')
         # criar models principais
         UF = get_model('imoveis', 'UF')
         self.uf = UF(nome='UF1')
-        self.uf._history_user = self.user
         self.uf.save()
 
         Municipio = get_model('imoveis', 'Municipio')
         self.municipio = Municipio(nome='Mun1', uf=self.uf)
-        self.municipio._history_user = self.user
         self.municipio.save()
 
         Bairro = get_model('imoveis', 'Bairro')
         self.bairro = Bairro(nome='B1', municipio=self.municipio)
-        self.bairro._history_user = self.user
         self.bairro.save()
 
     def test_sync_new_quadra(self):
