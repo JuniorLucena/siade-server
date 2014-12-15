68a272e07498d193468eedbd4cc27d1ba0dd9cfa
roldaojr <roldaogjr@gmail.com>
2014-12-15 15:56:32 -0300

    Correção do crud de agentes

--- siade/agentes/views/agente.py
+++ siade/agentes/views/agente.py
@@ -2,48 +2,52 @@
 from django.views.generic import (CreateView, ListView, UpdateView,
                                   DeleteView, DetailView)
 from django.core.urlresolvers import reverse_lazy
+from siade.utils.view_urls import registry
 from siade.mixins.messages import MessageMixin
 from ..models import Agente
 
 
-class ContextMixin(object):
-    def get_context_data(self, **kwargs):
-        context = super(ContextMixin, self).get_context_data(**kwargs)
-        context['title'] = self.model._meta.verbose_name.capitalize()
-
-        object_urls = {}
-        for n in ('listar', 'adicionar', 'detalhes', 'editar', 'excluir'):
-            object_urls[n] = 'agente-%s' % n
-
-        context['object_urls'] = object_urls
-        return context
-
-
-class CfgMixin(object):
+class AgenteMixin(object):
     model = Agente
     success_url = reverse_lazy('agente-listar')
     fields = ('cpf', 'nome', 'sobrenome', 'nascimento',
               'email', 'telefone', 'codigo', 'tipo', 'ativo')
 
+    def get_context_data(self, **kwargs):
+        context = super(AgenteMixin, self).get_context_data(**kwargs)
+        context['title'] = self.model._meta.verbose_name.capitalize()
+        context['object_class'] = self.model.__name__.lower()
+        return context
 
-class Listar(CfgMixin, ContextMixin, ListView):
+
+class Listar(AgenteMixin, ListView):
     pass
 
 
-class Adicionar(CfgMixin, ContextMixin, MessageMixin, CreateView):
+class Adicionar(AgenteMixin, MessageMixin, CreateView):
     success_message = u'Agente adicionado com êxito'
     template_name = 'crud/object_form.html'
 
 
-class Detalhes(CfgMixin, ContextMixin, DetailView):
+class Detalhes(AgenteMixin, DetailView):
     pass
 
 
-class Editar(CfgMixin, ContextMixin, MessageMixin, UpdateView):
+class Editar(AgenteMixin, MessageMixin, UpdateView):
     success_message = u'Agente atualizado com êxito'
     template_name = 'crud/object_form.html'
 
 
-class Excluir(CfgMixin, ContextMixin, MessageMixin, DeleteView):
+class Excluir(AgenteMixin, MessageMixin, DeleteView):
     success_message = u'Agente excluído com êxito'
     template_name = 'crud/object_confirm_delete.html'
+
+registry.register_actions(
+    'agente',
+    ('listar', Listar.as_view()),
+    ('adicionar', Adicionar.as_view()),
+    ('detalhes', Detalhes.as_view()),
+    ('editar', Editar.as_view()),
+    ('excluir', Excluir.as_view())
+)
+urls = registry.get_urls_for_model('agente')
