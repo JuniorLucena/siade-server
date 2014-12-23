a8f00ebf37fc4bae094999530249bbc694b74a9a
Roldão Rego Jr <roldaogjr@gmail.com>
2014-12-23 12:50:56 +0000

    Pequena correção

--- siade/wsgi.py
+++ siade/wsgi.py
@@ -9,5 +9,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siade.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
