d85ccd3d6f5f96366e22635ed393e88ef8d5dcf5
roldaojr <roldaogjr@gmail.com>
2014-12-24 18:48:48 -0300

    Atualização no código base
    Adicionado view para redefinir senha
    Menu agora verifica permissões

--- siade/base/urls.py
+++ siade/base/urls.py
@@ -1,21 +1,20 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
-from django.contrib.auth.views import password_change, password_change_done
-from django.contrib.auth.forms import AdminPasswordChangeForm
+from django.contrib.auth.views import (login, logout, password_change,
+                                       password_change_done)
+from .views import password_reset
 
 urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='home.html'),
         name='home'),
-    url(r'^usuario/redefinir-senha/$', password_change,
-        {'password_change_form': AdminPasswordChangeForm},
+    url(r'^usuario/redefinir-senha/(?P<user>\d+)/$', password_reset,
         name='password_reset'),
     url(r'^usuario/alterar-senha/$', password_change,
         name='password_change'),
     url(r'^usuario/senha-alterada/$', password_change_done,
         name='password_change_done'),
-    url(r'^usuario/login/$', 'django.contrib.auth.views.login',
-        name='user_login'),
-    url(r'^usuario/logout/$', 'django.contrib.auth.views.logout',
+    url(r'^usuario/login/$', login, name='user_login'),
+    url(r'^usuario/logout/$', logout, {'next_page': '/usuario/login/'},
         name='user_logout'),
 )
