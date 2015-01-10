2600294898bb98f671a3f4e908a467c84a8e1f6b
roldaojr <roldaogjr@gmail.com>
2015-01-09 22:34:45 -0300

    Incluido input_mask em agente
    Adicionado mascaras nos campos do cadastro de agente

---
+++ siade/agentes/forms.py
@@ -0,0 +1,32 @@
+import re
+from django import forms
+from django.utils.encoding import smart_text
+from localflavor.br.forms import BRPhoneNumberField, BRCPFField
+from input_mask.contrib.localflavor.br.widgets import (BRPhoneNumberInput,
+                                                       BRCPFInput)
+from .models import Agente
+
+
+class AgenteForm(forms.ModelForm):
+    cpf = BRCPFField(widget=BRCPFInput, label='CPF')
+    telefone = BRPhoneNumberField(widget=BRPhoneNumberInput, required=False)
+
+    def clean_cpf(self):
+        value = self.cleaned_data['cpf']
+        if not value.isdigit():
+            value = re.sub("[-\. ]", "", value)
+        return int(value)
+
+    def clean_telefone(self):
+        value = self.cleaned_data['telefone']
+        print value, type(value)
+        value = re.sub('-', '', smart_text(value))
+        if value == '':
+            return None
+        else:
+            return int(value)
+
+    class Meta:
+        model = Agente
+        fields = ('cpf', 'nome', 'sobrenome', 'nascimento',
+                  'email', 'telefone', 'codigo', 'tipo')
