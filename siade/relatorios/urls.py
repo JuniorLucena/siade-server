f6ab0125ca4694d137bd4f1da981798c93715140
Roldão Jr <roldaogjr@gmail.com>
2015-01-21 15:49:05 -0300

    Adicionado estatisticas de bairro

--- siade/relatorios/urls.py
+++ siade/relatorios/urls.py
@@ -3,6 +3,7 @@ import views
 
 urlpatterns = patterns(
     '',
+    url(r'^bairro/$', views.estatisticas_bairro),
     url(r'^quadra\.(\w+)$', views.rel_quadra),
     url(r'^pendencia\.(\w+)$', views.casas_pendentes),
     url(r'^diario/([0-9]{4})-([0-9]{2})-([0-9]+)\.(\w+)$', views.rel_diario),
--- siade/relatorios/views.py
+++ siade/relatorios/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.db.models import Sum, Count
 from .utils import html2pdf_link_callback
+from siade.imoveis.models import Quadra, Bairro
+from django import forms
 
 
 def render_response(templateName, data, context=None, fmt='pdf'):
@@ -47,3 +50,28 @@ def rel_diario(request, ano, mes, dia, fmt='pdf'):
 @login_required
 def rel_semanal(request, semana, fmt='pdf'):
     return render_response('relatorios/semana.rml', {}, fmt=fmt)
+
+
+def estatisticas_bairro(request):
+    bairros = [(bairro.id, bairro.nome) for bairro in Bairro.objects.all()]
+
+    class PesquisaForm(forms.Form):
+        bairro = forms.fields.ChoiceField(choices=bairros)
+
+    bairro_id = request.GET.get('bairro')
+    if bairro_id:
+        form = PesquisaForm(request.GET)
+        quadras = Quadra.objects.filter(bairro=bairro_id)
+        quadras = quadras.annotate(total_imoveis=Count('lados__imoveis')) \
+                         .annotate(pe=Sum('lados__imoveis__ponto_estrategico')) \
+                         .annotate(habitantes=Sum('lados__imoveis__habitantes')) \
+                         .annotate(caes=Sum('lados__imoveis__caes')) \
+                         .annotate(gatos=Sum('lados__imoveis__gatos'))
+    else:
+        form = PesquisaForm(request.GET)
+        quadras = []
+
+    return render(request, 'relatorios/bairro.html', {
+        'quadras': quadras,
+        'form': form,
+    })
