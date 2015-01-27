f2acd7b4b89e89b49515e373c3916854d521f779
Roldão Rego Jr <roldaogjr@gmail.com>
2015-01-27 11:51:13 +0000

    Atualizado configuração de teste

--- siade/settings/test.py
+++ siade/settings/test.py
@@ -3,10 +3,10 @@ from common import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['TEST_DB_NAME'],
-        'USER': os.environ['TEST_DB_USER'],
-        'PASSWORD': os.environ['TEST_DB_PASS'],
-        'HOST': os.environ['TEST_DB_HOST'],
-        'PORT': os.environ['TEST_DB_PORT']
+        'NAME': os.environ.get('TEST_DB_NAME', 'siade'),
+        'USER': os.environ.get('TEST_DB_USER', 'siade'),
+        'PASSWORD': os.environ.get('TEST_DB_PASS', ''),
+        'HOST': os.environ.get('TEST_DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('TEST_DB_PORT', 3306)
     }
 }
