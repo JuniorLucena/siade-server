f95502eecf78e33ad69bfb50ed7d7bad559a4da5
roldaojr <roldaogjr@gmail.com>
2015-02-15 22:06:19 -0300

    Atualizado sitetrees
    Colocado Agentes e Gerenciamento de ciclo como itens simples

--- siade/agentes/sitetrees.py
+++ siade/agentes/sitetrees.py
@@ -4,19 +4,16 @@ from sitetree.utils import tree, item
 
 dynamic_sitetrees = (
     tree('agentes', items=[
-        item('Agentes', '#', children=[
-            item('Cadastro de Agente', 'agentes:agente:listar', children=[
-                item('{{ agente.nome }}', 'agentes:agente:detalhes agente.id', children=[
-                        item('Alterar Agente', 'agentes:agente:editar agente.id',
-                             in_menu=False, in_sitetree=False,
-                             access_by_perms='agentes.change_agente'),
-                        item('Excluir Agente', 'agentes:agente:excluir agente.id',
-                             in_menu=False, in_sitetree=False,
-                             access_by_perms='agentes.delete_agente'),
-                ]),
-                item('Adicionar agente', 'agentes:agente:adicionar',
-                     access_by_perms='agentes.add_agente'),
-            ]),
+        item('Agentes', 'agentes:agente:listar', children=[
+            item('{{ agente.nome }}', 'agentes:agente:detalhes agente.id', children=[
+                    item('Alterar Agente', 'agentes:agente:editar agente.id',
+                         access_by_perms='agentes.change_agente'),
+                    item('Excluir Agente', 'agentes:agente:excluir agente.id',
+                         access_by_perms='agentes.delete_agente'),
+            ], in_menu=False, in_sitetree=False),
+            item('Adicionar agente', 'agentes:agente:adicionar',
+                 access_by_perms='agentes.add_agente',
+                 in_menu=False, in_sitetree=False),
         ]),
     ]),
 )
--- siade/trabalhos/sitetrees.py
+++ siade/trabalhos/sitetrees.py
@@ -4,15 +4,11 @@ from sitetree.utils import tree, item
 
 dynamic_sitetrees = (
     tree('trabalhos', items=[
-        item('Gerenciamento de ciclo', '', children=[
-            item('Situação do ciclo', 'ciclo:gerenciar'),
-
+        item('Gerenciamento de ciclo', 'ciclo:gerenciar', children=[
             item('Distribuir trabalhos', 'ciclo:distribuir_trabalhos',
                  in_menu=False, in_sitetree=False),
-
             item('Iniciar ciclo', 'ciclo:iniciar',
                  in_menu=False, in_sitetree=False),
-            
             item('Encerrar ciclo', 'ciclo:encerrar',
                  in_menu=False, in_sitetree=False),
         ], access_by_perms='trabalhos.change_ciclo')
