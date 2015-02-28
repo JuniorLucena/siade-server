7e75c622f741c0a6ea2422f2d6f0d36ad5e751dd
roldaojr <roldaogjr@gmail.com>
2015-02-28 00:36:02 -0300

    Aprimorada interface de distribuir trabalhos

--- siade/trabalhos/views.py
+++ siade/trabalhos/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from datetime import date
 from django.db.models import Count
 from django.views.generic import (UpdateView)
+from django.http.response import JsonResponse
 from django.shortcuts import redirect, render, get_object_or_404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
@@ -10,11 +11,12 @@ from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
 from django.forms.models import modelform_factory
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 from siade.trabalhos.forms import CicloDatePicker
 from .forms import IniciarCicloForm, TrabalhoForm
 from .models import Ciclo, Trabalho, Visita
 from siade.agentes.models import Agente
-from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from siade.imoveis.models import Bairro, Quadra
 
 
 @permission_required('trabalhos.change_ciclo', raise_exception=True)
@@ -26,11 +28,11 @@ def gerenciar_ciclo(request):
 
     agentes = Agente.objects.filter(tipo=Agente.Tipo.AgenteCampo)
 
-    imoveis = dict(Agente.objects.filter(trabalhos__ciclo=ciclo)\
-        .annotate(total=Count('trabalhos__quadra__lados__imoveis'))\
+    imoveis = dict(Agente.objects.filter(trabalhos__ciclo=ciclo)
+        .annotate(total=Count('trabalhos__quadra__lados__imoveis'))
         .values_list('id', 'total'))
 
-    vistas = dict(Agente.objects.filter(visitas__ciclo=ciclo)\
+    vistas = dict(Agente.objects.filter(visitas__ciclo=ciclo)
         .annotate(total=Count('visitas__imovel')).values_list('id', 'total'))
 
     for a in agentes:
@@ -43,7 +45,7 @@ def gerenciar_ciclo(request):
             a.percentual = 0
 
     context = {
-        'ciclo' : ciclo,
+        'ciclo': ciclo,
         'agentes': agentes
     }
     return render(request, 'trabalhos/gerenciar_ciclo.html', context)
@@ -101,33 +103,37 @@ def distribuir_trabalhos(request):
 
 
 @permission_required('trabalhos.change_ciclo', raise_exception=True)
-def trabalhos_remover(request, pk):
-    return_to = request.META.get('HTTP_REFERER', 'ciclo:distribuir_trabalhos')
-    get_object_or_404(Trabalho, pk=pk).delete()
-    return redirect(return_to)
-
-
-def trabalhos_alterar(request, *args, **kwargs):
-    agente_id = request.GET.get('agente') or request.POST.get('agente')
-    agente = get_object_or_404(Agente, pk=agente_id)
+def trabalhos_alterar(request, pk=None):
+    agente = get_object_or_404(Agente, pk=pk)
     if request.method == 'POST':
         form = TrabalhoForm(agente, request.POST)
         if form.is_valid():
-            # Guardar quantas quadras foram digitadas no form
-            n_quadras = len(form.cleaned_data.get('quadras'))
-            # salvar o form
+            success_url = request.POST.get('next', reverse('ciclo:distribuir_trabalhos'))
             form.save()
-            # Verificar quantas quadras foram relamente incluidas
-            n_incluidas = len(form.cleaned_data.get('quadras'))
-            # Se for diferente emitir um aviso
-            if n_quadras != n_incluidas:
-                messages.warning(request, 'Algumas quadras não foram incluídas.')
-
-            form = TrabalhoForm(agente)
+            messages.success(request, 'Quadras atualizadas com sucesso.')
+            return redirect(success_url)
     else:
         form = TrabalhoForm(agente)
-    context = {'form': form}
-    return context
+
+    context = {
+        'form': form,
+        'agente': agente,
+        'trabalhos': Trabalho.objects.filter(ciclo=Ciclo.atual(),
+                                             agente=agente),
+        'bairros': Bairro.objects.filter(municipio=agente.municipio),
+    }
+    return render(request, 'trabalhos/quadras_agente.html', context)
+
+
+@permission_required('trabalhos.change_ciclo', raise_exception=True)
+def trabalhos_quadras(request):
+    bairro = request.GET.get('bairro')
+    agente = request.GET.get('agente')
+    trabalhos = Trabalho.objects.filter(ciclo=Ciclo.atual())
+    exclude = trabalhos.exclude(agente=agente).values_list('quadra', flat=True)
+    quadras = Quadra.objects.filter(bairro=bairro).exclude(id__in=exclude)
+    return JsonResponse(list(quadras.values('id', 'numero', 'bairro')),
+                        safe=False)
 
 
 class AlterarCiclo(LoginRequiredMixin, PermissionRequiredMixin, UpdateView ):
@@ -140,7 +146,6 @@ class AlterarCiclo(LoginRequiredMixin, PermissionRequiredMixin, UpdateView ):
 
 
 def listar_imoveis_visitados(request, pk):
-
     agente = get_object_or_404(Agente, pk=pk)
 
     visitas = Visita.objects.all()\
